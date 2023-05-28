4f6af0696294d5d3ff0c4c22d4b5457cbf6f3571
yeison <ycuervob@unal.edu.co>
2023-05-27 19:05:55 -0500

    se añade script de python

---
+++ ejecuciones.py
@@ -0,0 +1,32 @@
+import csv
+import subprocess
+
+# Ejecutar el comando make
+try:
+    subprocess.run(["make", "clean"], check=True)
+    subprocess.run(["make"], check=True)
+    print("Make ejecutado exitosamente")
+except subprocess.CalledProcessError as e:
+    print("Error al ejecutar make:", e)
+
+# Ruta al ejecutable de C++
+ejecutable = "./programa.out"
+
+# Ruta al archivo CSV
+archivo_csv = "param.csv"
+
+# Leer el archivo CSV
+with open(archivo_csv, 'r') as archivo:
+    lector_csv = csv.reader(archivo, delimiter=';')
+
+    # Iterar sobre las filas del archivo CSV
+    for fila in lector_csv:
+        # Extraer los valores de cada columna
+        columna1, columna2, columna3 = fila
+
+        # Llamar al ejecutable de C++ con los argumentos
+        try:
+            subprocess.run([ejecutable, columna1, columna2, columna3], check=True)
+            print("Programa de C++ ejecutado exitosamente para la fila:", fila)
+        except subprocess.CalledProcessError as e:
+            print("Error al ejecutar el programa de C++ para la fila:", fila)
